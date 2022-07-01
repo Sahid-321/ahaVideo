26ed627ca3de0735824d2b10e558406831d53824
Sahid Jamal <75063419+Sahid-321@users.noreply.github.com>
2022-07-01 20:18:18 +0530

    logIn updated

--- JS/login.js
+++ JS/login.js
@@ -1,67 +1,47 @@
 
-
-
     document.querySelector("form").addEventListener("submit", submitFunc);
 
     document.querySelector("#mobile").addEventListener("click", submitFunc1);
     document.querySelector("#facebook").addEventListener("click", submitFunc2);
     document.querySelector("#google").addEventListener("click", submitFunc3);
 
-    function clearmessage(){
-      document.getElementById("pTag").innerText = "";
-    }
+
 
     function submitFunc(){
      event.preventDefault();
 
      var email = document.getElementById("email").value;
-     var password = document.getElementById("password").value;
+
      var detail = JSON.parse(localStorage.getItem("data"));
-     console.log(detail);
 
      if(email == ""){
       //window.location.href = "SignUp.html"
       var p = document.getElementById("pTag").innerText = "Please Enter a Valid Email";
+     }else{
+      window.location.href = "SignUp.html"
      }
-     else{
     
-    var check = false;
-    var emailcheck = false;
      detail.filter(function(elem){
-      if(elem.email == email){
-        emailcheck = true;
-      }
-      if( elem.email == email && elem.password == password){
-        // alert("success")
-        // window.location.href = "index.html";
-        check = true;
-      }
-      else{
-        // alert("Check Credentials");
-        check = false;
-      }
-     })
-     if(check){
-        alert("You are successfully logged in")
+
+      if( elem.email == email){
         window.location.href = "index.html";
-        // var name = document.createElement("div");
-        // name.innerText = email;
-        // name.innerText = email;
-        // windows.parent.document.getElementById("right").append(name);
-        
-     }
-     else{
-      if(emailcheck == false){
-        alert("Email Does Not found, Please SIGN-UP First");
-        window.location.href = "SignUp.html";
+        alert("success")
+   
       }
-      else{
-        alert("Wrong Credentials Entered");
-      }
+      
+      
+     })
+    
+
+
+   
+
+
+
+
+    
+  
     }
-        
-     }
-      }
     function submitFunc1(){
         alert("yes its mobile")
     }
