4da63b63fabf7e4fb2e83940740c25afc2fe7c37
Dheeraj joshi <djsays124@gmail.com>
2022-07-02 14:23:51 +0530

    sliding window added

--- kid.js
+++ kid.js
@@ -17,8 +17,22 @@ arrows.forEach((arrow,i)=>{
         
     })
     //console.log(movielist[i].querySelectorAll("img").length);
-    
-})
+})    
 
 
-
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
